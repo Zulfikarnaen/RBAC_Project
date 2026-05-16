dc8b2dcbc0a5e150571fea9059ff31989b9136c5
Aurellya <h1101241043@student.untan.ac.id>
2026-05-16 21:03:06 +0700

    .git

--- prisma.config.ts
+++ prisma.config.ts
@@ -11,6 +11,6 @@ export default defineConfig({
   },
   engine: "classic",
   datasource: {
-    url: env("DATABASE_URL"),
+    url: "mysql://avnadmin:AVNS_nYGWUQ2e8YKHr1ut_vu@rbac-mysql-rbac.k.aivencloud.com:11409/defaultdb?ssl-mode=REQUIRED"
   },
 });
